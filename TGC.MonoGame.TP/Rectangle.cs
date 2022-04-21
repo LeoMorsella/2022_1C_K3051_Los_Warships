770dcbfb26a310c8caf517d99e92ab31c1d6425d
unknown <leomorsella@frba.utn.edu.ar>
2022-04-21 18:03:30 -0300

    se logra generar rectangulo

--- TGC.MonoGame.TP/Rectangle.cs
+++ TGC.MonoGame.TP/Rectangle.cs
@@ -5,44 +5,40 @@ using Microsoft.Xna.Framework.Input;
 
 namespace TGC.MonoGame.TP;
 
-public class GameShip:Game
+public class Rectangle:Game
 {
-     public const string ContentFolder3D = "Models/";
+    public const string ContentFolder3D = "Models/";
         public const string ContentFolderEffects = "Effects/";
         public const string ContentFolderMusic = "Music/";
         public const string ContentFolderSounds = "Sounds/";
         public const string ContentFolderSpriteFonts = "SpriteFonts/";
         public const string ContentFolderTextures = "Textures/";
-        
+
+       /* private const float CarVelocity = 2f;
+        private const float CarRotatingVelocity = 0.06f;
+         */
+       private const float CarVelocity = 2f;
+       private const float CarRotatingVelocity = 0.06f;
+       private const float CarJumpSpeed = 150f;
+       private const float Gravity = 350f;
+       
+
         private GraphicsDeviceManager Graphics { get; }
         
-        private Model ShipModel { get; set; }
-        private Matrix ShipWorld { get; set; }
+        
         private FollowCamera FollowCamera { get; set; }
-        
-        private Matrix ShipScale { get; set; }
-        
-        private Vector3 ShipPosition { get; set; }
-    
-        
-        //Dibujar Mar
+       
         private BasicEffect _basicEffect;
         private VertexPositionColor[] _vertices;
         private short[] _indices;
         private Matrix _world = Matrix.Identity;
         private Matrix _view;
         private Matrix _projection;
-        private SpriteBatch _spriteBatch;        
+        private SpriteBatch _spriteBatch;
         
+
         
-        
-        
-       
-        
-        /// <summary>
-        ///     Constructor del juego
-        /// </summary>
-        public GameShip()
+        public Rectangle()
         {
             // Se encarga de la configuracion y administracion del Graphics Device
             Graphics = new GraphicsDeviceManager(this);
@@ -54,7 +50,10 @@ public class GameShip:Game
             IsMouseVisible = true;
         }
 
- 
+        /// <summary>
+        ///     Llamada una vez en la inicializacion de la aplicacion.
+        ///     Escribir aca todo el codigo de inicializacion: Todo lo que debe estar precalculado para la aplicacion.
+        /// </summary>
         protected override void Initialize()
         {
             // Enciendo Back-Face culling
@@ -63,27 +62,14 @@ public class GameShip:Game
             rasterizerState.CullMode = CullMode.CullCounterClockwiseFace;
             GraphicsDevice.RasterizerState = rasterizerState;
             GraphicsDevice.BlendState = BlendState.Opaque;
-
-            // Configuro el tamaño de la pantalla
-            Graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width - 100;
-            Graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height - 100;
-            Graphics.ApplyChanges();
-
+            
             // Creo una camaar para seguir a nuestro auto
             FollowCamera = new FollowCamera(GraphicsDevice.Viewport.AspectRatio);
             
-        
-
-            //Posicion del auto y matriz
-            // Configuro la matriz de mundo del auto
-            ShipWorld = Matrix.Identity;
-            
-            ShipScale = Matrix.CreateScale(0.05f);
-
-            
-            
-            // Mar
-            
+            Graphics.PreferredBackBufferWidth = 1920;
+            Graphics.PreferredBackBufferHeight = 1080;
+            Graphics.ApplyChanges();
+// TODO: Add your initialization logic here
             _vertices = new VertexPositionColor[4];
             _vertices[0] = new VertexPositionColor
             {
@@ -113,21 +99,20 @@ public class GameShip:Game
             _indices[3] = 2;
             _indices[4] = 3;
             _indices[5] = 0;
-        
+
             
+
             base.Initialize();
         }
 
-    
+        /// <summary>
+        ///     Llamada una sola vez durante la inicializacion de la aplicacion, luego de Initialize, y una vez que fue configurado GraphicsDevice.
+        ///     Debe ser usada para cargar los recursos y otros elementos del contenido.
+        /// </summary>
         protected override void LoadContent()
         {
+           
             
-
-            // La carga de contenido debe ser realizada aca
-            ShipModel = Content.Load<Model>(ContentFolder3D + "ShipA/Ship");
-
-            
-            // Mar
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             _basicEffect = new BasicEffect(GraphicsDevice);
@@ -140,7 +125,9 @@ public class GameShip:Game
             _basicEffect.View = _view;
             _basicEffect.Projection = _projection;
             _basicEffect.VertexColorEnabled = true;
-          
+           
+            
+
 
             base.LoadContent();
         }
@@ -161,13 +148,6 @@ public class GameShip:Game
                 // Salgo del juego
                 Exit();
 
-
-            ShipWorld = ShipScale;
-
-
-            // Actualizo la camara, enviandole la matriz de mundo del auto
-            FollowCamera.Update(gameTime, ShipWorld);
-
             base.Update(gameTime);
         }
         
@@ -178,17 +158,8 @@ public class GameShip:Game
         /// </summary>
         protected override void Draw(GameTime gameTime)
         {
-            // Limpio la pantalla
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
-       
-           
-            // El dibujo del auto debe ir aca
-            
-            
-            ShipModel.Draw(ShipWorld,FollowCamera.View,FollowCamera.Projection);
-            
-            
+// TODO: Add your drawing code here
             GraphicsDevice.SamplerStates[0] = SamplerState.PointClamp;
             foreach (EffectPass pass in _basicEffect.CurrentTechnique.Passes)
             {
@@ -196,14 +167,8 @@ public class GameShip:Game
                 GraphicsDevice.DrawUserIndexedPrimitives(PrimitiveType.TriangleList, _vertices,
                     0, 4, _indices, 0, 2);
             }
-
-           // Quad.Draw(FloorWorld*viewProjection);
             base.Draw(gameTime);
         }
-        
-        
-        
-        
 
         /// <summary>
         ///     Libero los recursos cargados
@@ -215,4 +180,4 @@ public class GameShip:Game
 
             base.UnloadContent();
         }
-}
+    }
